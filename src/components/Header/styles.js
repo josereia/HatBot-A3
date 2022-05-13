288d0c901bbc5372df18c646e7216f6a6d68b3f7
João Sereia <joao.lo.sereia@gmail.com>
2022-05-12 21:30:45 -0300

    Estrutura inicial de arquivos

---
+++ src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import styles from "styled-components";
+
+export const Container = styles.header`
+    width: 100%;
+    height: 60px;
+    padding: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: solid 1px #eee;
+`
