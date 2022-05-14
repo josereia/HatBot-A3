432ee3ece8542770169b115c6dc32f251f0a416f
João Sereia <joao.lo.sereia@gmail.com>
2022-05-13 22:02:03 -0300

    att

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,37 +3,37 @@ import { Container, ContentModal, Title, IconContainer } from './styles';
 import { AiOutlineDislike, AiOutlineLike, AiFillDislike, AiFillLike } from "react-icons/ai";
 
 const Modal = ({ children }) => {
-  const [selected, setSelected] = useState(null)
-  const [isVisisble, setIsVisible] = useState(true)
+	const [selected, setSelected] = useState(null)
+	const [isVisisble, setIsVisible] = useState(true)
 
-  const handleSelect = useCallback(index => {
-    setSelected(index)
-    setTimeout(() => {
-      setIsVisible(false)
-    }, 1000)
-  }, [selected])
+	const handleSelect = useCallback(index => {
+		setSelected(index)
+		setTimeout(() => {
+			setIsVisible(false)
+		}, 1000)
+	}, [selected])
 
-  return (
-    <>
-      {isVisisble && <Container>
-        <ContentModal>
-          <Title> O que achou do nosso atendimento?</Title>
-          <IconContainer>
+	return (
+		<>
+			{isVisisble && <Container>
+				<ContentModal>
+					<Title> O que achou do nosso atendimento?</Title>
+					<IconContainer>
 
-            {selected === 1 ?
-              (<AiFillLike size={40} color={'#fff'} />)
-              : (<AiOutlineLike size={40} color={'#fff'} onClick={() => handleSelect(1)} />)
-            }
+						{selected === 1 ?
+							(<AiFillLike size={40} color={'#fff'} />)
+							: (<AiOutlineLike size={40} color={'#fff'} onClick={() => handleSelect(1)} />)
+						}
 
-            {selected === 2 ?
-              (<AiFillDislike size={40} color={'#fff'} />)
-              : (<AiOutlineDislike size={40} color={'#fff'} onClick={() => handleSelect(2)} />)
-            }
+						{selected === 2 ?
+							(<AiFillDislike size={40} color={'#fff'} />)
+							: (<AiOutlineDislike size={40} color={'#fff'} onClick={() => handleSelect(2)} />)
+						}
 
-          </IconContainer>
-        </ContentModal>
-      </Container>}
-    </>
-  )
+					</IconContainer>
+				</ContentModal>
+			</Container>}
+		</>
+	)
 }
 export default Modal
